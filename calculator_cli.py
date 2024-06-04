fda39c3fc46055cbc74d70650f36d7b1977aca04
Jv2350 <tp8375735@gmail.com>
2024-06-05 00:04:54 +0530

    minor changes

--- calculator_cli.py
+++ calculator_cli.py
@@ -1,2 +1,18 @@
-expression = input("Enter the expression for the calculation: ")
-print(f"{expression}: {eval(expression)}")
+def main():
+    while True:  # to take infinite inputs
+        try:
+            # take input from the user
+            expression = input("Enter an arithmetic expression (or 'quit' to exit): ")
+
+            if expression.lower() == "quit":
+                break
+
+            result = eval(expression)
+            print(f"Result: {result}")
+
+        except Exception as e:
+            print(f"Error: {e}")
+
+
+if __name__ == "__main__":
+    main()
